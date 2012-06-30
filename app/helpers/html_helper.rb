3609307c231c590e5fdd0ce076ad0fd6e0b80f62
Angelo Maron <angelo.maron@akra.de>
2012-06-30 11:34:00 +0200

    first refactor to use own kaminari views

--- app/helpers/html_helper.rb
+++ app/helpers/html_helper.rb
@@ -3,4 +3,8 @@ module HtmlHelper
   def link_to_blank(name, link, options = {})
     link_to name, link, options.merge(target: "blank")
   end
+  
+  def active_class(condition, klass = "")
+    condition ? "#{klass} active".strip : klass
+  end
 end
