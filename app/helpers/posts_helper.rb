fb0a0602a73e673457e62ac75931e188d1fee745
Angelo Maron <angelo.maron@akra.de>
2012-07-17 00:00:57 +0200

    refactored show … now show link as text as additional information

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -3,6 +3,19 @@ module PostsHelper
   def link_to_category category
     link_to category.name, category_path(category), {:class => "jq_category_link", "data-tab-index" => category.id}
   end
+  
+  def post_as_link post
+    link = link_to_blank post.name, post.link
+    span = content_tag :span, "(#{truncate_link(post.link)})", class: "display_link"
+    
+    "#{link} #{span}".html_safe
+  end
+  
+  def truncate_link(link)
+    return "-" if link.nil?
+    truncate(link.gsub("https://", "").gsub("http://", ""), length: 60)
+  end
+  
 
   def post_information post
     post_information = content_tag :span, datetime_string(post.created_at), class: "date"
