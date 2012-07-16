3edda5c4e234d05d7bf14af5088d28d39d348e5c
Angelo Maron <angelo.maron@akra.de>
2012-07-16 18:44:59 +0200

    extended admin users admin for names

--- app/admin/admin_users.rb
+++ app/admin/admin_users.rb
@@ -1,6 +1,8 @@
 ActiveAdmin.register AdminUser do
   index do
     column :email
+    column :first_name
+    column :last_name
     column :current_sign_in_at
     column :last_sign_in_at
     column :sign_in_count
@@ -11,6 +13,8 @@ ActiveAdmin.register AdminUser do
     h3 admin_user.email
     attributes_table do
       row :email
+      row :first_name
+      row :last_name
       row :current_sign_in_at
       row :last_sign_in_at
       row :sign_in_count
@@ -21,6 +25,8 @@ ActiveAdmin.register AdminUser do
   form do |f|
     f.inputs "Admin Details" do
       f.input :email
+      f.input :first_name
+      f.input :last_name
       f.input :password
       f.input :password_confirmation
     end
