ebe0a71c0ddf896a617daef39dd0e1fd3f3eb035
Angelo Maron <angelo.maron@akra.de>
2012-07-16 18:50:00 +0200

    removed password from admin interface - find later better way

--- app/admin/admin_users.rb
+++ app/admin/admin_users.rb
@@ -27,8 +27,6 @@ ActiveAdmin.register AdminUser do
       f.input :email
       f.input :first_name
       f.input :last_name
-      f.input :password
-      f.input :password_confirmation
     end
     f.buttons
   end
